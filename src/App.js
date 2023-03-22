60b400b55f82acf8331a1b462f1ed2f94d1a841b
NayemEhsan <naymakon@gmail.com>
2023-03-22 21:01:22 +0600

    2nd commit

--- src/App.js
+++ src/App.js
@@ -2,21 +2,51 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <Product></Product>
+       <Counter></Counter>
     </div>
   );
 }
 
-function Product(props){
+function Counter(){
   return (
     <div>
-      <h3>Name :</h3>
-      <p>Price :</p>
+      <h1>Count :</h1>
     </div>
   )
 }
+
+// ...............................................
+// const products = [
+//   {name : 'laptop',price : 1500000},
+//   {name : 'phone',price : 100000},
+//   {name : 'watch',price : 15000},
+//   {name : 'sunglass',price : 3000},
+//   {name : 'shoes',price : 4000},
+//   {name : 'dress',price : 7000}
+// ]
+// ..............................................
+//....its included by returned div...uses
+// */
+// {
+//   products.map(product => <Product name ={product.name} price ={product.price}>
+    
+//   </Product>)
+// }
+// <Product name = "laptop" price = "40000"></Product>
+// <Product name = "phone" price = "70000"></Product>
+// <Product name = "watch" price = "3000"></Product>*/
+
+// function Product(props){
+//   return (
+//     <div className='product'>
+//       <h3>Name : {props.name}</h3>
+//       <p>Price : {props.price}</p>
+//     </div>
+//   )
+// }
 
 export default App;
  
